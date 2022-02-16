b251dae8b4fcea3a285d4bf9e9dcc2b6308ef959
DANNY <dannycaliz94@gmail.com>
2022-02-16 16:10:40 -0600

    buen estado

--- PRESENTACION/Form1.Designer.cs
+++ PRESENTACION/Form1.Designer.cs
@@ -29,16 +29,18 @@
         private void InitializeComponent()
         {
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.Report_plac_mes1 = new PRESENTACION.Report_plac_mes();
             this.SuspendLayout();
             // 
             // crystalReportViewer1
             // 
-            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.ActiveViewIndex = 0;
             this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
             this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ReportSource = this.Report_plac_mes1;
             this.crystalReportViewer1.Size = new System.Drawing.Size(862, 460);
             this.crystalReportViewer1.TabIndex = 0;
             // 
@@ -57,5 +59,6 @@
         #endregion
 
         private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private Report_plac_mes Report_plac_mes1;
     }
 }
