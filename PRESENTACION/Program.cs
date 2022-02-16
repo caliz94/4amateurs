807752cce0f024cdf7b2e1f240e3f4bbcb8032ac
DANNY <dannycaliz94@gmail.com>
2022-02-16 08:38:48 -0600

    buen estado

--- PRESENTACION/Program.cs
+++ PRESENTACION/Program.cs
@@ -16,7 +16,7 @@ namespace PRESENTACION
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CP_CONTENEDOR());
+            Application.Run(new Form1());
         }
     }
 }
